4a927271026d0a9cf03ed14f42f1682daa093ffc
chenkv <chen.kevin885@gmail.com>
2022-05-04 17:48:18 -0600

    Setup Tailwindcss

--- ramedia/components/Navbar.js
+++ ramedia/components/Navbar.js
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import styles from "../styles/Navbar.module.css";
+
 export default function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className="flex">
+            <div className="h-14" id={styles.logo} >
+                Image
+            </div>
+            <div className="grow" id={styles.test}>
+                Test
+            </div>
         </div>
     )
 }
