81238a56a68c815ff6ff3efcc1b87f543957b127
chenkv <chen.kevin885@gmail.com>
2022-07-15 22:59:33 -0600

    Made MovieOptions component do nothing when pressing watched button after movie is watched.

--- ramedia/components/MovieOptions.js
+++ ramedia/components/MovieOptions.js
@@ -91,38 +91,38 @@ export default function MovieOptions({ user, movieID }) {
           <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
         </svg>
       )
+
+      let temp = info;
+      temp.watched = !info.watched;
+      setInfo(temp);
+      console.log(info);
     } else {
-      var body = {
-        user: userInfo.res,
-        id: movieID
-      }
+      // var body = {
+      //   user: userInfo.res,
+      //   id: movieID
+      // }
 
-      const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body)
-      };
+      // const options = {
+      //   method: 'POST',
+      //   headers: {
+      //       'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(body)
+      // };
 
-      await fetch('/api/user/delete-watchlist', options);
-      button.disabled = false;
+      // await fetch('/api/user/delete-watchlist', options);
+      // button.disabled = false;
 
-      seenRoot.render(
-        <svg id="seenicon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" className={`h-12 w-12 ${styles.fadeIn}`}
-          preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="#ff971d" d="M19.447 5.345A3.27
-          3.27 0 0 0 16.29 3a3.293 3.293 0 0 0-3.277 3h-2.025a3.297 3.297 0 0 0-3.284-3a3.268 3.268 0 0 0-3.151
-          2.345l-2.511 8.368A1.027 1.027 0 0 0 2 14v1a5.006 5.006 0 0 0 5.001 5a5.003 5.003 0 0 0 4.576-3h.846a5.003
-          5.003 0 0 0 4.576 3A5.006 5.006 0 0 0 22 14.999V14c0-.098-.015-.194-.042-.287l-2.511-8.368zM7.001 18A3.005
-          3.005 0 0 1 4 15c0-.076.017-.147.022-.222A2.995 2.995 0 0 1 7 12a3 3 0 0 1 3 3v.009A3.004 3.004 0 0 1 7.001
-          18zm9.998 0A3.004 3.004 0 0 1 14 15.009V15a3 3 0 0 1 6-.001A3.005 3.005 0 0 1 16.999 18z"/></svg>
-      )
+      // seenRoot.render(
+      //   <svg id="seenicon" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" className={`h-12 w-12 ${styles.fadeIn}`}
+      //     preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="#ff971d" d="M19.447 5.345A3.27
+      //     3.27 0 0 0 16.29 3a3.293 3.293 0 0 0-3.277 3h-2.025a3.297 3.297 0 0 0-3.284-3a3.268 3.268 0 0 0-3.151
+      //     2.345l-2.511 8.368A1.027 1.027 0 0 0 2 14v1a5.006 5.006 0 0 0 5.001 5a5.003 5.003 0 0 0 4.576-3h.846a5.003
+      //     5.003 0 0 0 4.576 3A5.006 5.006 0 0 0 22 14.999V14c0-.098-.015-.194-.042-.287l-2.511-8.368zM7.001 18A3.005
+      //     3.005 0 0 1 4 15c0-.076.017-.147.022-.222A2.995 2.995 0 0 1 7 12a3 3 0 0 1 3 3v.009A3.004 3.004 0 0 1 7.001
+      //     18zm9.998 0A3.004 3.004 0 0 1 14 15.009V15a3 3 0 0 1 6-.001A3.005 3.005 0 0 1 16.999 18z"/></svg>
+      // )
     }
-
-    let temp = info;
-    temp.watched = !info.watched;
-    setInfo(temp);
-    console.log(info);
   }
 
   async function addToWatchlist() {
