ed5001d74f3e9f74f5f22a0f142e29ee668020ca
chenkv <chen.kevin885@gmail.com>
2022-08-08 15:00:42 -0600

    Made profile page a little better but no functionality

--- ramedia/components/profile/Menu.js
+++ ramedia/components/profile/Menu.js
@@ -14,30 +14,35 @@ export default function Menu({ user }) {
     </button>
   );
 
-  const updateUserInfo = async (event) => {
+  const updateUsername = async (event) => {
     event.preventDefault();
-    console.log(event.target.listname.value)
+    console.log(event.target.username.value)
 
-    var userInfo = await fetch(`/api/user/email/'${user.user.email}'`);
-    userInfo = await userInfo.json();
+    // var userInfo = await fetch(`/api/user/email/'${user.user.email}'`);
+    // userInfo = await userInfo.json();
 
-    let body = {
-      user: userInfo.res,
-      name: event.target.listname.value,
-      description: event.target.listdescription.value
-    }
+    // let body = {
+    //   user: userInfo.res,
+    //   name: event.target.listname.value,
+    //   description: event.target.listdescription.value
+    // }
 
-    const options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-    }
+    // const options = {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify(body),
+    // }
 
-    await fetch('/api/user/lists', options);
+    // await fetch('/api/user/lists', options);
+  }
 
-    modalHandler(false);
+  const test = async (event) => {
+    const currValue = event.target.value;
+    const inputDiv = document.getElementById('username');
+    const errorDiv = document.getElementById("username_error");
+    errorDiv.innerHTML = "Max length of 20 characters";
   }
 
   return (
@@ -53,10 +58,21 @@ export default function Menu({ user }) {
             if (option == 'profile') {
               return (
                 <div className="w-full m-4">
-                  <form className="" onSubmit={updateUserInfo}>
-                    <label htmlFor='nickname'>Name of list:</label>
-                    <input type='text' id='nickname' value={user.nickname} />
-                    <input type='submit' value='Submit' className=""/>
+                  <form className="flex flex-col space-y-4" onSubmit={updateUsername}>
+                    <div className="flex space-x-2 ml-1">
+                      <label htmlFor='username'>Username:</label>
+                      <input type='text' id='username' defaultValue={user.nickname} className='px-2 rounded-md focus:outline-2 focus:outline-[#FF971D]' onChange={test} maxLength={20} />
+                      <p id="username_error" className="text-red-500 font-semibold"></p>
+                    </div>
+
+                    <div className="flex space-x-2 ml-1">
+                      <label htmlFor='email'>Email:</label>
+                      <input type='text' id='email' defaultValue={user.email} className='px-2 rounded-md focus:outline-2 focus:outline-[#FF971D] w-1/3' disabled={true} />
+                    </div>
+
+                    <div className="w-full text-center">
+                      <input type='submit' value='Save Changes' className="w-min px-2 py-1 rounded-lg bg-[#FF971D] bg-opacity-80 hover:bg-opacity-30 transition-all ease-linear duration-150"/>
+                    </div>
                   </form>
                 </div>
               )
