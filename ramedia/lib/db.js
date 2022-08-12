55de2a914380acbf37e456910029202474b2dbe7
chenkv <chen.kevin885@gmail.com>
2022-08-12 11:44:47 -0600

    Fixed db function

--- ramedia/lib/db.js
+++ ramedia/lib/db.js
@@ -1,28 +1,28 @@
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const conn = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const conn = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-conn.connect();
-
-export default conn;
-
-// import { Pool } from "pg";
-
-// let conn;
-
-// if (!conn) {
-//   conn = new Pool({
-//     user: process.env.PGSQL_USER,
-//     password: process.env.PGSQL_PASSWORD,
-//     host: process.env.PGSQL_HOST,
-//     port: process.env.PGSQL_PORT,
-//     database: process.env.PGSQL_DATABASE,
-//   });
-// }
+// conn.connect();
 
 // export default conn;
+
+import { Pool } from "pg";
+
+let conn;
+
+if (!conn) {
+  conn = new Pool({
+    user: process.env.PGSQL_USER,
+    password: process.env.PGSQL_PASSWORD,
+    host: process.env.PGSQL_HOST,
+    port: process.env.PGSQL_PORT,
+    database: process.env.PGSQL_DATABASE,
+  });
+}
+
+export default conn;
